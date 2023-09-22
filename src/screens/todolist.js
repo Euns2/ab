5b1ccbac322ce825fc7d23e0303c987b63eb07e3
은세 <80892967+Euns2@users.noreply.github.com>
2023-09-22 11:23:32 +0900

    Merge branch 'master' of https://github.com/Euns2/ab

--- src/screens/todolist.js
+++ src/screens/todolist.js
@@ -2,32 +2,33 @@ import { StatusBar } from "expo-status-bar";
 import react, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-//테스트1
-// const STORAGE_KEY = "@toDos";
+
+const STORAGE_KEY = "@toDos";
 export default function App() {
   const [text, setText] = useState("");
-  // const [toDos, setToDos] = useState({});
-  // useEffect(() => {
-  //   loadToDos();
-  // }, []);
-  // const onChangeText = (payload) => setText(payload);
+  const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadToDos();
+  }, []);
+  const onChangeText = (payload) => setText(payload);
   // //saveToDos는 @toDos와 함께 AsyncStorage에 저장
-  // const saveToDos = async (toSave) => {
-  //   await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave)); //저장할 saveToDos를 string으로 바꿔서 저장
-  // };
-  // const loadToDos = async () => {
-  //   const s = await AsyncStorage.getItem(STORAGE_KEY);
-  //   setToDos(JSON.parse(s));
-  // };
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave)); //저장할 saveToDos를 string으로 바꿔서 저장
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s) || {});    //초기 상태가 빈 객체인 경우, loadToDos 함수에서 null 대신에 빈 객체를 반환하도록 처리
+  };
   const addToDo = async () => {
     if (text === "") {
       return;
     }
-    //   const newToDos = { ...toDos, [Date.now()]: { text } };
-    //   setToDos(newToDos);
-    //   await saveToDos(newToDos);
-    //   setText("");
+      const newToDos = { ...toDos, [Date.now()]: { text } };
+      setToDos(newToDos);
+      await saveToDos(newToDos);
+      setText("");
   };
+
 
   return (
     <View style={styles.container}>
@@ -36,44 +37,62 @@ export default function App() {
         <Text>To do</Text>
       </View>
       <TextInput
-        // onSubmitEditing={addToDo}
-        // onChangeText={onChangeText}
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
         value={text}
         placeholder={"+ To Do List"}
         style={styles.input}
       />
-      {/* <ScrollView>
+      <ScrollView>
         {Object.keys(toDos).map((key) => (
           <View style={styles.toDo} key={key}>
-            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+          <Text style={styles.toDoText}>{toDos[key].text}</Text>
           </View>
         ))}
-      </ScrollView> */}
+      </ScrollView>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
     paddingHorizontal: 20,
   },
 
+  header: {
+    marginTop: 20,
+    marginLeft: 5
+  },
+
   input: {
-    backgroundColor: "gray",
-    paddingVertical: 15,
+    backgroundColor: "#F5F5F5",
+    paddingVertical: 10,
     paddingHorizontal: 20,
-    borderRadius: 20,
+    borderRadius: 15,
     marginTop: 20,
     fontSize: 18,
   },
 
   toDo: {
-    marginBottom: 10,
-    paddingVertical: 20,
-    paddingHorizontal: 40,
+    marginBottom: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    borderBottomColor: "#868686",
+    borderBottomWidth: 2,
+    // width: "80%",
+  },
+
+  checkbox: {
+    width: 24,
+    height: 24,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginLeft: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   toDoText: {
